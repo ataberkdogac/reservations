180fb4bdf137d4a723e534769df33eb40dbabaa4
Ataberk <ataberkdgc@gmail.com>
2025-10-03 18:18:50 +0300

    modal text issues

--- src/components/v1/vifs/myModal.vue
+++ src/components/v1/vifs/myModal.vue
@@ -20,23 +20,23 @@ then(function (response) {
     <div id = "myModal" class="myModal">
         
             <div class="modal-content">
-                <label for = "name" required>Name:</label>
-                <input type="text" id="name"></input>
+                <label for = "name" >Name:</label>
+                <input type="text" id="name" placeholder="Name"></input>
                 <label for = "surname">Surname:</label>
-                <input type="text" id="surname"></input>
+                <input type="text" id="surname" placeholder="Surname"></input>
                 <label for="start_date">Start Date:</label>
                 <input type="date" id="start_date"></input>
                 <label for="end_date" required>End Date:</label>
                 <input type="date" id="end_date"></input>
                 <label for="total_fee"> Total Fee:</label>
-                <input type="text" id="total_fee"></input>
+                <input type="text" id="total_fee" placeholder="Amount"></input>
                 <label>Status:</label>
                 <input type="radio" id="appr" name = "stt" >Approved</input><br>
                 <input type="radio" id="pend" name = "stt">Pending</input><br>
                 <input type="radio" id="cacl" name = "stt">Cancelled</input><br>
                 <label>Hotel Name:</label>
                 <select id="hotid">
-                <option value="">Select an Hotal...</option>
+                <option value="">Select an Hotel...</option>
                 </select>
                 <button id="closeModal" class="closeModal">Save</button>
             </div>
@@ -46,12 +46,12 @@ then(function (response) {
 <style>
     .myModal {
         display: none;
-        background-color: rgba(0, 0, 0, 0.8);
+        background-color: rgba(0,0,0, 0.8);
         width: 100%;
         height: 100%;
         position: fixed; 
         z-index: 1;
-        
+        color: black;
     }
 
     .modal-content{
@@ -60,13 +60,15 @@ then(function (response) {
         min-width: 20%;
         margin: auto;
         margin-top: 6%;
-        background-color: black;
+        background-color: rgb(255, 202, 26);
         padding: 10px;
         
     }
 
     input {
         margin: 10px;
+        background-color: rgb(255, 238, 140);
+        color: black;
     }
 
     label {
@@ -77,13 +79,19 @@ then(function (response) {
 
     button {
         display:block;
-        
         margin: 10px;
+        
     }
 
     select{
         margin: 5px;
+        background-color: rgb(255, 238, 140);
+        color: black;
     }
     
+    .radio{
+        background-color: rgb(255, 238, 140);
+        color: black;
+    }
     
 </style>
